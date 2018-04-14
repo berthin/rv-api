ca6a9899fc9fc3c572432fbd0995077c7ef16678
BERTHIN TORRES <berthin@BERTHINs-MacBook-Pro.local>
2018-04-14 17:36:56 -0300

    Convert tabs to spaces

--- main.go
+++ main.go
@@ -2,29 +2,29 @@ package main
 
 
 import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"time"
+    "database/sql"
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "time"
 
-	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/dgrijalva/jwt-go/request"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+    jwt "github.com/dgrijalva/jwt-go"
+    "github.com/dgrijalva/jwt-go/request"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/gorilla/handlers"
+    "github.com/gorilla/mux"
 )
 
 
 const (
-	DBUSER     = "root"
-	DBPASSWORD = "1234"
-	DBHOST     = "127.0.0.1"
-	DBPORT     = "8080"
-	DBBASE     = "redventures"
-	PORT       = ":8081"
+    DBUSER     = "root"
+    DBPASSWORD = "1234"
+    DBHOST     = "127.0.0.1"
+    DBPORT     = "8080"
+    DBBASE     = "redventures"
+    PORT       = ":8081"
 )
 
 
@@ -33,51 +33,51 @@ const (
 // ****************
 
 type Message struct {
-	Message string `json:"message"`
+    Message string `json:"message"`
 }
 
 
 func panicOnError(err error) {
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 }
 
 
 func logOnError(err error) {
-	if err != nil {
-		log.Println(err)
-	}
+    if err != nil {
+        log.Println(err)
+    }
 }
 
 
 func sendHttpMessage(message interface{}, statusCode int, w http.ResponseWriter) {
-	msg, err := json.Marshal(message)
-	logOnError(err)
+    msg, err := json.Marshal(message)
+    logOnError(err)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	w.Write(msg)
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(statusCode)
+    w.Write(msg)
 }
 
 
 func isTokenAccessCorrect(w http.ResponseWriter, r *http.Request) bool {
-	token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor,
-		func(token *jwt.Token) (interface{}, error) {
-			return mySigningKey, nil
-		})
-		
-	if err != nil {
-		sendHttpMessage(Message{"Unauthorized access"}, http.StatusBadRequest, w)
-		return false
-	}
-	
-	if !token.Valid {
-		sendHttpMessage(Message{"Invalid token"}, http.StatusBadRequest, w)
-		return false
-	}
-		
-	return true
+    token, err := request.ParseFromRequest(r, request.AuthorizationHeaderExtractor,
+        func(token *jwt.Token) (interface{}, error) {
+            return mySigningKey, nil
+        })
+        
+    if err != nil {
+        sendHttpMessage(Message{"Unauthorized access"}, http.StatusBadRequest, w)
+        return false
+    }
+    
+    if !token.Valid {
+        sendHttpMessage(Message{"Invalid token"}, http.StatusBadRequest, w)
+        return false
+    }
+        
+    return true
 }
 
 
@@ -86,44 +86,44 @@ var db *sql.DB
 
 
 func init() {
-	var err error
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", 
+    var err error
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", 
                        DBUSER, DBPASSWORD, DBHOST, DBPORT, DBBASE)
-	log.Println(dsn)
+    log.Println(dsn)
 
-	db, err = sql.Open("mysql", dsn)
-	panicOnError(err)
+    db, err = sql.Open("mysql", dsn)
+    panicOnError(err)
 
-	panicOnError(db.Ping())
+    panicOnError(db.Ping())
 
-	log.Println("Database connected!")
+    log.Println("Database connected!")
 }
 
 
 func main() {
-	router := mux.NewRouter()
+    router := mux.NewRouter()
 
-	router.HandleFunc("/api/v1/users",
-					  ListUsers).Methods(http.MethodGet)
-	router.HandleFunc("/api/v1/users/{id:[0-9]+}", 
-					  ListUserById).Methods(http.MethodGet)
+    router.HandleFunc("/api/v1/users",
+                      ListUsers).Methods(http.MethodGet)
+    router.HandleFunc("/api/v1/users/{id:[0-9]+}", 
+                      ListUserById).Methods(http.MethodGet)
 
-	router.HandleFunc("/api/v1/widgets", 
-					  ListWidgets).Methods(http.MethodGet)
-	router.HandleFunc("/api/v1/widgets", 
-					  CreateWidget).Methods(http.MethodPost)
-	router.HandleFunc("/api/v1/widgets/{id:[0-9]+}", 
-					  GetWidget).Methods(http.MethodGet)
-	router.HandleFunc("/api/v1/widgets/{id:[0-9]+}", 
-					  UpdateWidget).Methods(http.MethodPut)
+    router.HandleFunc("/api/v1/widgets", 
+                      ListWidgets).Methods(http.MethodGet)
+    router.HandleFunc("/api/v1/widgets", 
+                      CreateWidget).Methods(http.MethodPost)
+    router.HandleFunc("/api/v1/widgets/{id:[0-9]+}", 
+                      GetWidget).Methods(http.MethodGet)
+    router.HandleFunc("/api/v1/widgets/{id:[0-9]+}", 
+                      UpdateWidget).Methods(http.MethodPut)
 
-	router.HandleFunc("/api/v1/auth", 
-					  GenToken).Methods(http.MethodGet)
+    router.HandleFunc("/api/v1/auth", 
+                      GenToken).Methods(http.MethodGet)
 
-	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
+    loggedRouter := handlers.LoggingHandler(os.Stdout, router)
 
-	log.Print("Listening on PORT " + PORT)
-	log.Fatal(http.ListenAndServe(PORT, handlers.RecoveryHandler()(loggedRouter)))
+    log.Print("Listening on PORT " + PORT)
+    log.Fatal(http.ListenAndServe(PORT, handlers.RecoveryHandler()(loggedRouter)))
 }
 
 // ***************
@@ -131,7 +131,7 @@ func main() {
 // ***************
 
 type Token struct {
-	Token string `json:"token"`
+    Token string `json:"token"`
 }
 
 
@@ -139,22 +139,22 @@ var mySigningKey = []byte("4e3Fh54w374w")
 
 
 func GenToken(w http.ResponseWriter, r *http.Request) {
-	token := jwt.New(jwt.SigningMethodHS256)
+    token := jwt.New(jwt.SigningMethodHS256)
 
-	// @TODO: Check what are these lines used for? Seems that claims is not used. By testing with
-	// 		  GET, POST requests nothing changed when these were removed.
-	claims := token.Claims.(jwt.MapClaims)
-	claims["admin"] = true
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+    // @TODO: Check what are these lines used for? Seems that claims is not used. By testing with
+    // 		  GET, POST requests nothing changed when these were removed.
+    claims := token.Claims.(jwt.MapClaims)
+    claims["admin"] = true
+    claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 
-	/* Sign the token with our secret */
-	tokenString, err := token.SignedString(mySigningKey)
-	logOnError(err)
+    /* Sign the token with our secret */
+    tokenString, err := token.SignedString(mySigningKey)
+    logOnError(err)
 
-	authToken := Token{}
-	authToken.Token = tokenString
+    authToken := Token{}
+    authToken.Token = tokenString
 
-	sendHttpMessage(authToken, http.StatusOK, w)
+    sendHttpMessage(authToken, http.StatusOK, w)
 }
 
 
@@ -163,72 +163,72 @@ func GenToken(w http.ResponseWriter, r *http.Request) {
 // ***************
 
 type User struct {
-	Id       int64  `json:"id"`
-	Name     string `json:"name"`
-	Gravatar string `json:"gravatar,omitempty"`
+    Id       int64  `json:"id"`
+    Name     string `json:"name"`
+    Gravatar string `json:"gravatar,omitempty"`
 }
 
 
 
 func ListUsers(w http.ResponseWriter, r *http.Request) {
-	if !isTokenAccessCorrect(w, r) {
-		return
-	}
+    if !isTokenAccessCorrect(w, r) {
+        return
+    }
 
-	rows, err := db.Query(`
-		SELECT id, name, gravatar
-		FROM user 
-		ORDER BY user.id DESC`)
-	logOnError(err)
-	defer rows.Close()
+    rows, err := db.Query(`
+        SELECT id, name, gravatar
+        FROM user 
+        ORDER BY user.id DESC`)
+    logOnError(err)
+    defer rows.Close()
 
-	users := make([]User, 0)
-	for rows.Next() {
-		user := User{}
+    users := make([]User, 0)
+    for rows.Next() {
+        user := User{}
 
-		err := rows.Scan(
-			&user.Id, 
-			&user.Name, 
-			&user.Gravatar)
-		logOnError(err)
+        err := rows.Scan(
+            &user.Id, 
+            &user.Name, 
+            &user.Gravatar)
+        logOnError(err)
 
-		users = append(users, user)
-	}
+        users = append(users, user)
+    }
 
-	logOnError(rows.Err())
-	sendHttpMessage(users, http.StatusOK, w)
+    logOnError(rows.Err())
+    sendHttpMessage(users, http.StatusOK, w)
 }
 
 
 func ListUserById(w http.ResponseWriter, r *http.Request) {
-	if !isTokenAccessCorrect(w, r) {
-		return
-	}
+    if !isTokenAccessCorrect(w, r) {
+        return
+    }
 
-	vars := mux.Vars(r)
-	pathParam := vars["id"]
+    vars := mux.Vars(r)
+    pathParam := vars["id"]
 
-	log.Println("id" + pathParam)
-	row := db.QueryRow(`
-		SELECT id, name, gravatar 
-		FROM user 
-		WHERE user.id = ?`, pathParam)
+    log.Println("id" + pathParam)
+    row := db.QueryRow(`
+        SELECT id, name, gravatar 
+        FROM user 
+        WHERE user.id = ?`, pathParam)
 
-	user := User{}
-	err := row.Scan(
-		&user.Id, 
-		&user.Name, 
-		&user.Gravatar)
-	log.Println(user.Name)
+    user := User{}
+    err := row.Scan(
+        &user.Id, 
+        &user.Name, 
+        &user.Gravatar)
+    log.Println(user.Name)
 
-	if err == sql.ErrNoRows {
-		message := Message{"The record for the user couldn't be found."}
-		sendHttpMessage(message, http.StatusBadRequest, w)
-		return
-	}
-	logOnError(err)
+    if err == sql.ErrNoRows {
+        message := Message{"The record for the user couldn't be found."}
+        sendHttpMessage(message, http.StatusBadRequest, w)
+        return
+    }
+    logOnError(err)
 
-	sendHttpMessage(user, http.StatusOK, w)
+    sendHttpMessage(user, http.StatusOK, w)
 }
 
 
@@ -237,181 +237,181 @@ func ListUserById(w http.ResponseWriter, r *http.Request) {
 // ***************
 
 type Widget struct {
-	Id        int64   `json:"id"`
-	Name      string  `json:"name"`
-	Color     string  `json:"color,omitempty"`
-	Price     float32 `json:"price"`
-	Melts     bool    `json:"melts"`
-	Inventory int64   `json:"inventory"`
+    Id        int64   `json:"id"`
+    Name      string  `json:"name"`
+    Color     string  `json:"color,omitempty"`
+    Price     float32 `json:"price"`
+    Melts     bool    `json:"melts"`
+    Inventory int64   `json:"inventory"`
 }
 
 
 func ListWidgets(w http.ResponseWriter, r *http.Request) {
-	if !isTokenAccessCorrect(w, r) {
-		return
-	}
+    if !isTokenAccessCorrect(w, r) {
+        return
+    }
 
-	rows, err := db.Query(`
-		SELECT id, name, color, price, melts, inventory 
-		FROM widget 
-		ORDER BY widget.id DESC`)
-	logOnError(err)
-	defer rows.Close()
+    rows, err := db.Query(`
+        SELECT id, name, color, price, melts, inventory 
+        FROM widget 
+        ORDER BY widget.id DESC`)
+    logOnError(err)
+    defer rows.Close()
 
-	widgets := make([]Widget, 0)
-	for rows.Next() {
-		widget := Widget{}
+    widgets := make([]Widget, 0)
+    for rows.Next() {
+        widget := Widget{}
 
-		err := rows.Scan(
-			&widget.Id, 
-			&widget.Name, 
-			&widget.Color, 
-			&widget.Price, 
-			&widget.Melts, 
-			&widget.Inventory)
-		logOnError(err)
+        err := rows.Scan(
+            &widget.Id, 
+            &widget.Name, 
+            &widget.Color, 
+            &widget.Price, 
+            &widget.Melts, 
+            &widget.Inventory)
+        logOnError(err)
 
-		widgets = append(widgets, widget)
-	}
-	logOnError(rows.Err())
+        widgets = append(widgets, widget)
+    }
+    logOnError(rows.Err())
 
-	sendHttpMessage(widgets, http.StatusOK, w)
+    sendHttpMessage(widgets, http.StatusOK, w)
 }
 
 
 func GetWidget(w http.ResponseWriter, r *http.Request) {
-	if !isTokenAccessCorrect(w, r) {
-		return
-	}
+    if !isTokenAccessCorrect(w, r) {
+        return
+    }
 
-	vars := mux.Vars(r)
-	pathParam := vars["id"]
+    vars := mux.Vars(r)
+    pathParam := vars["id"]
 
-	// Improve select to return in the column order fashion
-	row := db.QueryRow(`
-		SELECT id, name, color, price, melts, inventory 
-		FROM widget 
-		WHERE widget.id = ?`, pathParam) 
+    // Improve select to return in the column order fashion
+    row := db.QueryRow(`
+        SELECT id, name, color, price, melts, inventory 
+        FROM widget 
+        WHERE widget.id = ?`, pathParam) 
 
-	widget := Widget{}
-	err := row.Scan(
-		&widget.Id,
-		&widget.Name, 
-		&widget.Color, 
-		&widget.Price, 
-		&widget.Melts, 
-		&widget.Inventory)
+    widget := Widget{}
+    err := row.Scan(
+        &widget.Id,
+        &widget.Name, 
+        &widget.Color, 
+        &widget.Price, 
+        &widget.Melts, 
+        &widget.Inventory)
 
-	if err == sql.ErrNoRows {
-		message := Message{"The record for the widget couldn't be found."}
-		sendHttpMessage(message, http.StatusBadRequest, w)
-		return
-	}
-	logOnError(err)
+    if err == sql.ErrNoRows {
+        message := Message{"The record for the widget couldn't be found."}
+        sendHttpMessage(message, http.StatusBadRequest, w)
+        return
+    }
+    logOnError(err)
 
-	sendHttpMessage(widget, http.StatusOK, w)
+    sendHttpMessage(widget, http.StatusOK, w)
 }
 
 
 func CreateWidget(w http.ResponseWriter, r *http.Request) {
-	if !isTokenAccessCorrect(w, r) {
-		return;
-	}
+    if !isTokenAccessCorrect(w, r) {
+        return;
+    }
 
-	widget := Widget{}
+    widget := Widget{}
 
-	err := json.NewDecoder(r.Body).Decode(&widget)
+    err := json.NewDecoder(r.Body).Decode(&widget)
 
-	if err != nil {
-		logOnError(err)
-		message := Message{"Something wrong happens. Try again later."}
-		sendHttpMessage(message, http.StatusBadRequest, w)
-		return
-	}
-	
-	if widget.Name == "" {
-		message := Message{"Name is required."}
-		sendHttpMessage(message, http.StatusNotAcceptable, w)
-		return
-	}
+    if err != nil {
+        logOnError(err)
+        message := Message{"Something wrong happens. Try again later."}
+        sendHttpMessage(message, http.StatusBadRequest, w)
+        return
+    }
+    
+    if widget.Name == "" {
+        message := Message{"Name is required."}
+        sendHttpMessage(message, http.StatusNotAcceptable, w)
+        return
+    }
 
-	stmt, err := db.Prepare(`
-		INSERT INTO widget 
-		(name, color, price, melts, inventory) 
-		VALUES (?, ?, ?, ?, ?)`)
-	logOnError(err)
-	defer stmt.Close()
+    stmt, err := db.Prepare(`
+        INSERT INTO widget 
+        (name, color, price, melts, inventory) 
+        VALUES (?, ?, ?, ?, ?)`)
+    logOnError(err)
+    defer stmt.Close()
 
-	record, err := stmt.Exec(
-		&widget.Name, 
-		&widget.Color, 
-		&widget.Price, 
-		&widget.Melts, 
-		&widget.Inventory)
-	logOnError(err)
+    record, err := stmt.Exec(
+        &widget.Name, 
+        &widget.Color, 
+        &widget.Price, 
+        &widget.Melts, 
+        &widget.Inventory)
+    logOnError(err)
 
-	lastRecord, err := record.LastInsertId()
-	logOnError(err)
+    lastRecord, err := record.LastInsertId()
+    logOnError(err)
 
-	rowNum, err := record.RowsAffected()
-	logOnError(err)
+    rowNum, err := record.RowsAffected()
+    logOnError(err)
 
-	log.Printf("Widget Created :: Record id %d :: Rows Affected %d", lastRecord, rowNum)
+    log.Printf("Widget Created :: Record id %d :: Rows Affected %d", lastRecord, rowNum)
 
-	message := Message{"Widget created successfully"}
-	sendHttpMessage(message, http.StatusOK, w)
+    message := Message{"Widget created successfully"}
+    sendHttpMessage(message, http.StatusOK, w)
 }
 
 
 func UpdateWidget(w http.ResponseWriter, r *http.Request) {
-	if !isTokenAccessCorrect(w, r) {
-		return
-	}
+    if !isTokenAccessCorrect(w, r) {
+        return
+    }
 
-	widget := Widget{}
-	err := json.NewDecoder(r.Body).Decode(&widget)
+    widget := Widget{}
+    err := json.NewDecoder(r.Body).Decode(&widget)
 
-	if err != nil {
-		logOnError(err)
-		message := Message{"Something wrong happens. Try again later."}
-		sendHttpMessage(message, http.StatusBadRequest, w)
-		return
-	}
+    if err != nil {
+        logOnError(err)
+        message := Message{"Something wrong happens. Try again later."}
+        sendHttpMessage(message, http.StatusBadRequest, w)
+        return
+    }
 
-	if widget.Name == "" {
-		message := Message{"Name is required."}
-		sendHttpMessage(message, http.StatusNotAcceptable, w)
-		return
-	}
+    if widget.Name == "" {
+        message := Message{"Name is required."}
+        sendHttpMessage(message, http.StatusNotAcceptable, w)
+        return
+    }
 
-	stmt, err := db.Prepare(`
-		UPDATE widget 
-		SET widget.name = ?, 
-			widget.color = ?,
-			widget.price = ?, 
-			widget.melts = ?, 
-			widget.inventory = ?
-		WHERE widget.id = ?`)
-	logOnError(err)
-	defer stmt.Close()
+    stmt, err := db.Prepare(`
+        UPDATE widget 
+        SET widget.name = ?, 
+            widget.color = ?,
+            widget.price = ?, 
+            widget.melts = ?, 
+            widget.inventory = ?
+        WHERE widget.id = ?`)
+    logOnError(err)
+    defer stmt.Close()
 
-	vars := mux.Vars(r)
-	pathParam := vars["id"]
+    vars := mux.Vars(r)
+    pathParam := vars["id"]
 
-	record, err := stmt.Exec(
-		&widget.Name, 
-		&widget.Color, 
-		&widget.Price, 
-		&widget.Melts, 
-		&widget.Inventory, 
-		pathParam)
-	logOnError(err)
+    record, err := stmt.Exec(
+        &widget.Name, 
+        &widget.Color, 
+        &widget.Price, 
+        &widget.Melts, 
+        &widget.Inventory, 
+        pathParam)
+    logOnError(err)
 
-	rowNum, err := record.RowsAffected()
-	logOnError(err)
+    rowNum, err := record.RowsAffected()
+    logOnError(err)
 
-	log.Printf("Widget Updated :: Rows Affected %d", rowNum)
+    log.Printf("Widget Updated :: Rows Affected %d", rowNum)
 
-	message := Message{"Widget updated successfully"}
-	sendHttpMessage(message, http.StatusOK, w)
+    message := Message{"Widget updated successfully"}
+    sendHttpMessage(message, http.StatusOK, w)
 }
